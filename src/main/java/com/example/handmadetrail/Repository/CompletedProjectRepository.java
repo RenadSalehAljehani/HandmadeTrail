7cad3f1509dafbe90e75fd7ed82095d04d96f79f
renad <renad.aljehani0@gmail.com>
2024-12-10 14:49:43 +0300

    Done

---
+++ src/main/java/com/example/handmadetrail/Repository/CompletedProjectRepository.java
@@ -0,0 +1,17 @@
+package com.example.handmadetrail.Repository;
+
+import com.example.handmadetrail.Model.CompletedProject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface CompletedProjectRepository extends JpaRepository<CompletedProject, Integer> {
+    // Using find
+    CompletedProject findOngoingProjectByCompletedProjectId(Integer completedProjectId);
+
+    List<CompletedProject> findCompletedProjectsByDiyBeginnerId(Integer diyBeginnerId);
+}
